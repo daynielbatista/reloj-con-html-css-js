294b27542bf7f1ca504c26561a568cf8474f6c1f
Dayniel <daynielbatista@gmail.com>
2020-05-01 01:02:40 +0100

    Reloj Finished

--- reloj.js
+++ reloj.js
@@ -1,16 +1,17 @@
 (function(){
 
     var actualizarHora = function(){
+        
         var fecha = new Date(),
             horas = fecha.getHours(),
             ampm,
             minutos = fecha.getMinutes(),
-            segundos = fecha.getSeconds(),
-            diaSemana = fecha.getDay(),            
+            segundos = fecha.getSeconds();
+           var diaSemana = fecha.getDay(),
             dia = fecha.getDate(),
             mes = fecha.getMonth(),
             year = fecha.getFullYear();
-
+            //console.log(diaSemana);
             
         var pHoras = document.getElementById('horas'),
             pAMPM = document.getElementById('ampm'),
@@ -22,11 +23,45 @@
             pYear = document.getElementById('year');
 
         var semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
-        pDiaSemana.textContent = diaSemana;
+        pDiaSemana.textContent = semana[diaSemana];
+       
+        pDia.textContent = dia;
+
+        var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+        pMes.textContent = meses[mes];
+
+        pYear.textContent = year;
+
+        if (horas >= 12) {
+            horas = horas -12;
+            ampm = 'PM';
+        } else{
+            ampm = 'AM';            
+        }
+
+        if (horas ==0) {
+            horas = 12;
+        }
+
+        pHoras.textContent = horas;
+        pAMPM.textContent = ampm;
+
+        if (minutos < 10) {
+            minutos = "0"+ minutos;
+        }
+
+        if (segundos < 10) {
+            segundos = "0" + segundos;
+        }
+
+
+        pMinutos.textContent = minutos;
+        pSegundos.textContent = segundos;
+
 
     };
 
     actualizarHora();
-    
+    var intervalo = setInterval(actualizarHora, 1000);
 
-})
+}())
